52837e158026a1c672d9b2405731932e7a52a20f
Alucard163 <romemix163@gmail.com>
2021-06-25 10:15:37 +0400

    Правки

--- models/course.js
+++ models/course.js
@@ -11,16 +11,18 @@ class Course {
     }
 
     toJSON() {
-        return JSON.stringify({
+        return {
             title: this.title,
             price: this.price,
             img: this.img,
             id: this.id
-        })
+        }
     }
     async save() {
         const courses = await Course.getAll();
         courses.push(this.toJSON());
+
+        console.log('Courses', courses);
 
         return new Promise((resolve, reject) => {
             fs.writeFile(
@@ -35,25 +37,48 @@ class Course {
                 }
         ) 
         })
-        
-
-        console.log('Courses', courses);
     }
 
     static getAll() {
-        return new Promise((resolve, reject) => {
+        return new Promise((res, rej) => {
             fs.readFile(
-                path.join(__dirname, '..', 'data', 'courses.json'), 'utf8',
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                'utf-8',
                 (err, content) => {
                     if (err) {
-                        reject(err);
+                        rej(err);
                     } else {
-                        resolve(JSON.parse(content))
+                        res(JSON.parse(content))
                     }
-    
                 }
             )
         })
+    }
+
+    static async getById(id) {
+        const courses = await Course.getAll()
+        return courses.find((c) => c.id === id)
+    }
+
+    static async update(course) {
+        const courses = await Course.getAll();
+
+        const idx = courses.findIndex(c => c.id === course.id)
+        courses[idx] = course;
+
+        return new Promise((res, rej) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                err => {
+                    if (err) {
+                        rej(err)
+                    } else {
+                        res()
+                    }
+                }
+            )
+        });
     }
 }
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,46 @@
-const {Router} = require('express');
-const router = Router();
+const express = require('express');
+const router = express.Router();
+const Course = require('../models/course');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
+    const courses = await Course.getAll()
+
     res.render('courses', {
-        title:'Курсы',
-        isCourses: true
-    })
+        title: 'Courses',
+        isCourses: true,
+        courses: courses
+    });
+})
+
+router.get('/:id/edit', async (req, res) => {
+
+    if (!req.query.allow) {
+        return res.redirect('/')
+    }
+
+    const course = await Course.getById(req.params.id)
+
+    res.render('course-edit', {
+        title: `Edit the course ${course.title}`,
+        course
+    });
+})
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body)
+    res.redirect('/courses')
+});
+
+router.get('/:id', async (req, res) => {
+
+    const course = await Course.getById(req.params.id)
+
+    res.render('course', {
+        layout: 'empty',
+        title: `Курс ${course.title}`,
+        course
+    });
 })
 
 module.exports = router;
